53485614b78288f0c86baf6e26fb58918897fd65
IvanEscamilla <Naruxhinal2054192ws>
2016-12-14 00:02:38 -0600

    a

--- scanner.js
+++ scanner.js
@@ -5,8 +5,8 @@ var async = require('async');
 var socket = require('socket.io-client')('http://localhost/scanner');
 
 //replace with your hardware address
-var serviceUuidToTrack = 'E6ED937950CF4C2687A8DE1D4B75BDAB';
-var characteristicUuidToRead = '551A74EA928D439B9225AE3CCC275822';
+var serviceUuidToTrack = 'e6ed937950cf4c2687a8de1d4b75bdab';
+var characteristicUuidToRead = '551a74ea928d439b9225ae3ccc275822';
 var peripheralToRead = "TestPeripheral";
 var allowDuplicates = false; // default: false
 
